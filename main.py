bdea7c2ea49d6dbcfb5936e3d3a6220c9ac3fbcd
tung <nguyenlamtung10@gmail.com>
2024-03-04 18:39:40 -0700

    first commit: BIC and linear

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 import pandas as pd
 import numpy as np
-
 from joblib import Parallel, delayed
 
 from utility_functions import get_acc, record
 from BIC import BIC
 from linear import linear_evaluate
 
-# PATHs (edit these paths depending on dataset)
-dataset = 'detailed'
 
+def get_stat(dataset):
 
-# training data
-inputs_path = 'training_data/' + dataset + '/inputs.csv'
-outputs_path = 'training_data/' + dataset + '/outputs.csv'
-evaluation_path = 'training_data/' + dataset + '/evaluation.csv'
+    # training data
+    inputs_path = 'training_data/' + dataset + '/inputs.csv'
+    outputs_path = 'training_data/' + dataset + '/outputs.csv'
+    evaluation_path = 'training_data/' + dataset + '/evaluation.csv'
 
-# writing accuracy rate path
-acc_rate_path = 'acc_rate/' + dataset + '.csv'
+    # writing accuracy rate path
+    acc_rate_path = 'acc_rate/' + dataset + '.csv'
 
-# path to write df to csv
-output_df_path = 'record_dataframe/' + dataset + '/'
+    # path to write df to csv
+    output_df_path = 'record_dataframe/' + dataset + '/'
 
-
-
-if __name__ == "__main__":
     # raw dfs
     evaluation_df = pd.read_csv(evaluation_path)
     inputs_df = pd.read_csv(inputs_path)
@@ -59,3 +54,8 @@ if __name__ == "__main__":
         acc_test_fold1 = get_acc(evaluation_fold1_df, lldas_test_fold1_df)
         acc_test_fold2 = get_acc(evaluation_fold2_df, lldas_test_fold2_df)
         record(method, lldas_test_fold1_df, lldas_test_fold2_df, acc_test_fold1, acc_test_fold2, output_df_path, acc_rate_path)
+
+
+stat_dataset_1 = delayed(get_stat)('detailed')
+stat_dataset_2 = delayed(get_stat)('systematic')
+stat_dataset_1, stat_dataset_2 = Parallel(n_jobs=2)([stat_dataset_1, stat_dataset_2])
