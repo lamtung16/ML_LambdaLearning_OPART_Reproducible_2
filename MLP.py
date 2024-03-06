3188d17adce6258cdc7d5809dace0afc8d814ccb
tung <nguyenlamtung10@gmail.com>
2024-03-06 09:59:11 -0700

    update cv

--- MLP.py
+++ MLP.py
@@ -91,10 +91,77 @@ def mlp_training(inputs_df, outputs_df, hidden_layers, hidden_size, chosen_featu
 
 
 
+def cv_learn(n_splits, X, y, n_hiddens, layer_size, batch_size, n_ites):
+    
+    # Define the K-fold cross-validation
+    kf = KFold(n_splits)
+
+    # loss function
+    loss_func = SquaredHingeLoss()
+
+    # learn best ite
+    total_losses = {'subtrain': np.zeros(n_ites), 'val': np.zeros(n_ites)}
+    data_splits = {'X_subtrain': [], 'X_val': [], 'y_subtrain': [], 'y_val': []}
+    
+    for subtrain_idx, val_idx in kf.split(X):
+
+        # Split the data into training and validation sets
+        indices = {'subtrain': subtrain_idx, 'val': val_idx}
+        for key in data_splits.keys():
+            feature_target, set_type = key.split('_')       # (X or y) and (subtrain or val)
+            data_splits[key].append(X[indices[set_type]] if feature_target == 'X' else y[indices[set_type]])
+
+        # Create DataLoader
+        dataset    = TensorDataset(data_splits['X_subtrain'][-1], data_splits['y_subtrain'][-1])
+        dataloader = DataLoader(dataset, batch_size, shuffle=False)
+
+        # Define your model
+        model = MLPModel(X.shape[1], n_hiddens, layer_size)
+
+        # define optimizer
+        optimizer = optim.Adam(model.parameters())
+
+        # Training loop for the specified number of iterations
+        for i in range(n_ites):
+            # training
+            for inputs, labels in dataloader:
+                optimizer.zero_grad()
+                loss = loss_func(model(inputs), labels)
+                loss.backward()
+                optimizer.step()
+
+            # validating
+            model.eval()
+            with torch.no_grad():
+                val_loss = loss_func(model(data_splits['X_val'][-1]), data_splits['y_val'][-1])
+
+            # add train_loss and val_loss into arrays
+            total_losses['val'][i] += val_loss.item()
+
+    best_no_ite = np.argmin(total_losses['val'])
+    return best_no_ite + 1
+
+
+
 # get lldas
 def mlp_evaluate(input_train_df, output_train_df, inputs_val_df, hidden_layers, hidden_size, chosen_feature, f_engineer, n_ites):
+    # cross-validation to get best number of iterations
+    
+    # cv inputs
+    cv_inputs = input_train_df[chosen_feature].to_numpy()
+    # feature engineering
+    for i in range(len(f_engineer)):
+        cv_inputs[:, i] = f_engineer[i](cv_inputs[:, i])
+    cv_inputs = torch.Tensor(cv_inputs)
+    
+    # cv output
+    targets_low  = torch.Tensor(output_train_df['min.log.lambda'].to_numpy().reshape(-1,1))
+    targets_high = torch.Tensor(output_train_df['max.log.lambda'].to_numpy().reshape(-1,1))
+    cv_outputs = torch.cat((targets_low, targets_high), dim=1)
+    best_n_ite = cv_learn(2, cv_inputs, cv_outputs, hidden_layers, hidden_size, 1, n_ites)
+    
     # trained model
-    model = mlp_training(input_train_df, output_train_df, hidden_layers, hidden_size, chosen_feature, f_engineer, n_ites)
+    model = mlp_training(input_train_df, output_train_df, hidden_layers, hidden_size, chosen_feature, f_engineer, best_n_ite)
     
     # test inputs
     inputs = inputs_val_df[chosen_feature].to_numpy()
