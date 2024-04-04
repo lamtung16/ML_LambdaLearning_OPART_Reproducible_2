2d42ae2a9cef19717ad2f0e9c54947b3e583a641
lamtung16 <nguyenlamtung10@gmail.com>
2024-04-04 14:23:22 -0700

    modify code MLP

--- main_MLP.py
+++ main_MLP.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import random
 from joblib import Parallel, delayed
 
 from utility_functions import get_acc, add_row_to_csv
@@ -8,7 +9,7 @@ from MLP import mlp_evaluate
 
 
 # dataset
-for dataset in ['cancer', 'detailed', 'systematic']:
+for dataset in ['detailed', 'systematic', 'cancer']:
 
     # training data
     fold_path = 'training_data/' + dataset + '/folds.csv'
@@ -39,22 +40,55 @@ for dataset in ['cancer', 'detailed', 'systematic']:
 
 
 
-    def get_fold_dfs(fold):
-        train_inputs_df = inputs_df[inputs_df['sequenceID'].isin(fold_df[fold_df['fold'] != fold]['sequenceID'])]
-        train_outputs_df = outputs_df[outputs_df['sequenceID'].isin(fold_df[fold_df['fold'] != fold]['sequenceID'])]
-        train_eval_df = evaluation_df[evaluation_df['sequenceID'].isin(fold_df[fold_df['fold'] != fold]['sequenceID'])]
-        test_inputs_df = inputs_df[inputs_df['sequenceID'].isin(fold_df[fold_df['fold'] == fold]['sequenceID'])]
-        test_eval_df = evaluation_df[evaluation_df['sequenceID'].isin(fold_df[fold_df['fold'] == fold]['sequenceID'])]
-        return train_inputs_df, train_outputs_df, train_eval_df, test_inputs_df, test_eval_df
+    def get_fold_dfs(n_fold, test_fold):
+        # get indices
+        train_folds = list(range(1, n_fold+1))
+        train_folds.remove(test_fold)
+
+        val_fold = random.choice(train_folds)
+        
+        subtrain_folds = train_folds
+        subtrain_folds.remove(val_fold)
+
+        # train
+        train_inputs_df  = inputs_df[inputs_df['sequenceID'].isin(fold_df[fold_df['fold'] == train_folds]['sequenceID'])]
+        train_outputs_df = outputs_df[outputs_df['sequenceID'].isin(fold_df[fold_df['fold'] == train_folds]['sequenceID'])]
+        train_eval_df    = evaluation_df[evaluation_df['sequenceID'].isin(fold_df[fold_df['fold'] == train_folds]['sequenceID'])]
+
+
+        # subtrain
+        subtrain_inputs_df  = inputs_df[inputs_df['sequenceID'].isin(fold_df[fold_df['fold'] == subtrain_folds]['sequenceID'])]
+        subtrain_outputs_df = outputs_df[outputs_df['sequenceID'].isin(fold_df[fold_df['fold'] == subtrain_folds]['sequenceID'])]
+        subtrain_eval_df    = evaluation_df[evaluation_df['sequenceID'].isin(fold_df[fold_df['fold'] == subtrain_folds]['sequenceID'])]
+
+        # validation
+        val_inputs_df = inputs_df[inputs_df['sequenceID'].isin(fold_df[fold_df['fold'] == val_fold]['sequenceID'])]
+        val_outputs_df = outputs_df[outputs_df['sequenceID'].isin(fold_df[fold_df['fold'] == val_fold]['sequenceID'])]
+        val_eval_df = evaluation_df[evaluation_df['sequenceID'].isin(fold_df[fold_df['fold'] == val_fold]['sequenceID'])]
+
+        # test
+        test_inputs_df = inputs_df[inputs_df['sequenceID'].isin(fold_df[fold_df['fold'] == test_fold]['sequenceID'])]
+        test_eval_df = evaluation_df[evaluation_df['sequenceID'].isin(fold_df[fold_df['fold'] == test_fold]['sequenceID'])]
+        return train_inputs_df, train_outputs_df, subtrain_inputs_df, subtrain_outputs_df, subtrain_eval_df, val_inputs_df, val_eval_df, test_inputs_df, test_eval_df
 
 
 
 
-    def process_combination(fold, n_layer, layer_size, feature_dict, normalize):
-        train_inputs_df, train_outputs_df, _, test_inputs_df, test_eval_df = get_fold_dfs(fold)
+    def process_combination(n_fold, test_fold, n_layer, layer_size, feature_dict, normalize):
+        train_inputs_df, train_outputs_df, subtrain_inputs_df, subtrain_outputs_df, _, val_inputs_df, val_eval_df, test_inputs_df, test_eval_df = get_fold_dfs(n_fold, test_fold)
         chosen_feature = feature_dict['chosen_feature']
         f_engineer = feature_dict['f_engineer']
         is_f_engineer = 1 if f_engineer[0] != identity else 0
+        lldas_eval_df = mlp_evaluate(
+            input_train_df=subtrain_inputs_df,
+            output_train_df=subtrain_outputs_df,
+            inputs_val_df=val_inputs_df,
+            hidden_layers=n_layer,
+            hidden_size=layer_size,
+            chosen_feature=chosen_feature,
+            f_engineer=f_engineer,
+            normalize=normalize
+        )
         lldas_test_df = mlp_evaluate(
             input_train_df=train_inputs_df,
             output_train_df=train_outputs_df,
@@ -65,8 +99,9 @@ for dataset in ['cancer', 'detailed', 'systematic']:
             f_engineer=f_engineer,
             normalize=normalize
         )
-        acc = get_acc(test_eval_df, lldas_test_df)
-        return [fold, n_layer, layer_size, chosen_feature, is_f_engineer, normalize, acc]
+        val_acc = get_acc(val_eval_df, lldas_eval_df)
+        test_acc = get_acc(test_eval_df, lldas_test_df)
+        return [test_fold, n_layer, layer_size, chosen_feature, is_f_engineer, normalize, val_acc, test_acc]
 
 
 
@@ -88,13 +123,13 @@ for dataset in ['cancer', 'detailed', 'systematic']:
     # linear
     linear_results = Parallel(n_jobs=-1)(
         delayed(process_combination)(
-            fold, 0, 1, feature_dict, 0
-        ) for fold in range(1, n_folds + 1)
+            n_folds, test_fold, 0, 1, feature_dict, 0
+        ) for test_fold in range(1, n_folds + 1)
         for feature_dict in feature_dict_list
     )
 
     for row in linear_results:
-        add_row_to_csv(acc_rate_path, ['fold', 'n_layer', 'layer_size', 'features', 'f_engineer', 'normalize', 'acc'], row)
+        add_row_to_csv(acc_rate_path, ['fold', 'n_layer', 'layer_size', 'features', 'f_engineer', 'normalize', 'val_acc', 'test_acc'], row)
 
 
 
@@ -102,12 +137,12 @@ for dataset in ['cancer', 'detailed', 'systematic']:
     # non linear
     mlp_results = Parallel(n_jobs=-1)(
         delayed(process_combination)(
-            fold, n_layer, layer_size, feature_dict, 1
-        ) for fold in range(1, n_folds + 1)
+            n_folds, test_fold, n_layer, layer_size, feature_dict, 1
+        ) for test_fold in range(1, n_folds + 1)
         for n_layer in n_layer_list
         for layer_size in layer_size_list
         for feature_dict in feature_dict_list
     )
 
     for row in mlp_results:
-        add_row_to_csv(acc_rate_path, ['fold', 'n_layer', 'layer_size', 'features', 'f_engineer', 'normalize', 'acc'], row)
+        add_row_to_csv(acc_rate_path, ['fold', 'n_layer', 'layer_size', 'features', 'f_engineer', 'normalize', 'val_acc', 'test_acc'], row)
